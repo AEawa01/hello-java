73fa193f8790f896e64f49c7598289a7e19efaaa
AEawa01 <aalawami@luc.edu>
2018-06-06 04:17:44 -0500

    first commit

---
+++ HelloWorld/src/helloPackage/Main.java
@@ -0,0 +1,8 @@
+package helloPackage;
+                                                 // @AEawa01
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello, COMP271!");
+    }
+}
